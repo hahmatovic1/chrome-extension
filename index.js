c4e06577992d38823cf8bad4a9c3cd07fe555cad
hahmatovic1 <72711477+hahmatovic1@users.noreply.github.com>
2022-08-31 12:15:21 +0200

    feat: Add function to display notes

--- index.js
+++ index.js
@@ -3,10 +3,20 @@ const notesList = document.getElementById("notes-list");
 const inputEl = document.getElementById("input-el");
 
 let notes = [];
+
+function showNotes(){
+    let listNotes = "";
+    for (let i = 0; i < notes.length; i++){
+        listNotes += `<li>${notes[i]}</li>`;
+    }
+    notesList.innerHTML = listNotes;
+}
+
+
 inputBtn.addEventListener("click", function(){
     notes.push(inputEl.value);
     inputEl.value = "";
-    
+    showNotes()
 })
 
 
